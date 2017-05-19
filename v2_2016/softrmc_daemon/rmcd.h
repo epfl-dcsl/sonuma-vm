9ab01a323c2e4f7419b2c8eb3be39d127ecee40e
root <root@nslrack56.epfl.ch>
2017-05-19 11:38:59 -0400

    added rmcd.h

---
+++ v2_2016/softrmc_daemon/rmcd.h
@@ -0,0 +1,96 @@
+/*
+ * Scale-Out NUMA Open Source License
+ *
+ * Copyright (c) 2017, Parallel Systems Architecture Lab, EPFL
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the Parallel Systems Architecture Lab, EPFL,
+ *   nor the names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE PARALLEL SYSTEMS ARCHITECTURE LAB,
+ * EPFL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  All-software implementation of the RMC
+ *
+ *  Authors: 
+ *  	Stanko Novakovic <stanko.novakovic@epfl.ch>
+ */
+
+#ifndef SOFT_RMC_H
+#define SOFT_RMC_H
+
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+
+#include "RMCdefines.h"
+
+#define IP_LEN 16
+#define PORT 7000
+
+#define MAX_NODE_CNT 24
+#define MAX_REGION_PAGES 4096
+
+#define RUNMAP 0
+#define RMAP 1
+#define GETREF 2
+#define PUTREF 3
+#define MR_ALLOC 4
+
+#define BILLION 1000000000
+
+#ifndef DEBUG_RMC
+#define DLog(M, ...)
+#else
+#define DLog(M, ...) fprintf(stdout, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#endif
+
+typedef struct server_info {
+  int nid;
+  int domid;
+  int fd;
+  char ip[IP_LEN];
+} server_info_t;
+
+typedef struct ioctl_info {
+  int op;
+  int node_id;
+  int domid;
+  int desc_gref;
+  unsigned long ctx;
+} ioctl_info_t;
+
+#endif
